e6e374214f301791923feafa3c7a5f3773184ee2
Shadman Shihab <shadmanshihab.cse8@gmail.com>
2025-04-24 15:46:47 +0600

    First commit with Simple.py

--- Simple.py
+++ Simple.py
@@ -1,4 +1,4 @@
 # hello.py
-print("Hello GitHub! I'm Shihab, future ML engineer 🚀")
+print("Hello GitHub! I am Shihab, future ML engineer 🚀")
 print("This is an update from My PC ")
 print("Another Update just for Testing")
